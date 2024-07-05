ac7dfa1b7cc895df054664ab404733005d20c579
gustavo-r-nunes <gustavo.r.nunes97@gmail.com>
2024-07-04 22:58:45 -0300

    bugfiox

--- src/main/java/me/dio/santander_dev_week_2024/SantanderDevWeek2024Application.java
+++ src/main/java/me/dio/santander_dev_week_2024/SantanderDevWeek2024Application.java
@@ -1,9 +1,12 @@
 package me.dio.santander_dev_week_2024;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(servers= {@Server(url = "/", description = "Default server url" )})
 public class SantanderDevWeek2024Application {
 
 	public static void main(String[] args) {
